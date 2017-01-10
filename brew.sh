422f3dd0507033ff5c1e8b0891d1de39c4b80f6f
Sven Lombaert <sven.lombaert@gmail.com>
2017-01-10 21:33:44 +0100

    Added brew file

---
+++ brew.sh
@@ -0,0 +1,33 @@
+# Ask for the administrator password upfront.
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until the script has finished.
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+# Make sure we’re using the latest Homebrew
+brew update
+
+# Upgrade any already-installed formulae
+brew upgrade --all
+
+# Taps
+brew tap homebrew/php
+
+# Install
+brew install php71
+brew install python
+brew install python3
+brew install composer
+brew install curl
+
+# Install apps
+brew cask install docker
+brew cask install kitematic
+brew cask install vlc
+brew cask install viscosity
+brew cask install dropbox
+brew cask install kibako
+brew cask install jumpcut
+
+# Clean
+brew cleanup
